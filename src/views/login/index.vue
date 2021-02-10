47427c5a71fabac6e3bf571026c8b570229c3063
c4az6 <1026037632@qq.com>
2021-02-10 14:04:01 +0800

    完成登录页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <!-- nav-bar部分 start -->
+    <div class="nav-bar-wrap">
+      <van-nav-bar
+      title="注册/登录"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    </div>
+    <!-- nav-bar部分 end -->
+
+    <!-- 表单部分 start -->
+    <van-cell-group>
+      <van-field
+        v-model="user.mobile"
+        icon-prefix="toutiao"
+        clearable
+        left-icon="shouji"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user.code"
+        clearable
+        icon-prefix="toutiao"
+        left-icon="yanzhengma"
+        placeholder="请输入验证码"
+      >
+        <template #button>
+          <van-button class="send-code" size="small" @click="handleSendCode">发送验证码</van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
+    <!-- 表单部分 end -->
+    <div class="login-btn-wrap">
+      <van-button class="login-btn" block  @click="handleSendCode">登录</van-button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,14 +44,37 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    // 发送验证码函数
+    handleSendCode () {
+      console.log('发送验证码...')
+    }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+  .login-btn-wrap {
+    padding: 20px;
+    .login-btn {
+      background-color: #6db4fb;
+      color: #fff;
+    }
+  }
+
+  .send-code {
+    background-color: #6db4fb;
+    color: #fff;
+  }
+</style>
