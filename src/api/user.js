4d5e48472b96893380ebaaabbb7ac1d137280706
c4az6 <1026037632@qq.com>
2021-02-26 13:25:24 +0800

    我的-使用请求兰即诶起统一设置token

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,4 @@
 import request from '@/utils/request'
-import store from '@/store'
 /* 用户相关请求模块 */
 export const userLogin = data => {
   return request({
@@ -21,9 +20,6 @@ export const sendSms = data => {
 export const getCurrentUser = data => {
   return request({
     url: '/app/v1_0/user',
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    method: 'GET'
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,21 @@
 /* 封装的axios请求模块 */
 
 import axios from 'axios'
+import store from '@/store'
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn'
+})
+
+request.interceptors.request.use(config => {
+  const { user } = store.state
+  // 如果用户已登录，统一给接口设置 token 信息
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  // 处理完请求之后一定要把config返回，否则请求就会停在这里
+  return config
+}, error => {
+  return Promise.reject(error)
 })
 
 export default request
