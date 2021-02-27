0ebef9c1665466bc73ff4ea7057297aff3264d5c
c4az6 <1026037632@qq.com>
2021-02-28 04:15:33 +0800

    首页文章列表项处理相对时间和样式

--- src/components/article-item.vue
+++ src/components/article-item.vue
@@ -18,7 +18,7 @@
         <div class="label-wrap">
           <span>{{ article.aut_name }}</span>
           <span>{{ article.comm_count }}评论</span>
-          <span>{{ article.pubdate }}</span>
+          <span>{{ article.pubdate | relativeTime}}</span>
         </div>
       </div>
       <!-- 默认插槽名字叫 default，可以省略不写 -->
@@ -59,6 +59,12 @@ export default {
   color: #3a3a3a;
 }
 
+.van-cell__title {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
 /deep/ .van-cell__value {
   flex: unset;
   width: 116px;
@@ -81,5 +87,30 @@ export default {
       padding-right: 4px;
     }
   }
+}
+
+.label-wrap {
+  font-size: 11px;
+  color: #b4b4b4;
+  span {
+    margin-right: 12px;
+  }
+}
+
+.van-cell:not(:last-child)::after {
+    position: absolute;
+    box-sizing: border-box;
+    content: ' ';
+    pointer-events: none;
+    right: 0;
+    bottom: 0;
+    left: 0.42667rem;
+    border-bottom: 0.02667rem solid #ebedf0;
+    -webkit-transform: scaleY(.5);
+    transform: scaleY(.5);
+}
+
+.van-cell:last-child::after {
+    display: block;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import Vant from 'vant'
 // 载入动态设置rem基准值的插件
 import 'amfe-flexible'
 import 'vant/lib/index.css'
+// 加载处理相对时间的插件
+import './utils/dayjs'
 // 加载全局样式
 import './style/index.less'
 Vue.config.productionTip = false
