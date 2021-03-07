019c1fd555ea65634df455fdea8be0fe4057c515
c4az6 <1026037632@qq.com>
2021-03-07 20:41:36 +0800

    创建文章详情页面并配置路由

---
+++ src/views/article/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="article-container">
+    文章详情组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
