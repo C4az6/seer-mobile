084993bbf3edbb30d558f6db0a88b5736d3baa71
c4az6 <1026037632@qq.com>
2021-03-12 19:26:22 +0800

    评论模块接口异常，做完其他模块再来做

--- src/views/article/components/comment-reply.vue
+++ src/views/article/components/comment-reply.vue
@@ -1,60 +1,110 @@
 <template>
   <div class="comment-reply">
-    <van-nav-bar
-      title="2条回复"
-      @click-left="$emit('close')"
-    >
+    <van-nav-bar :title="comment.reply_count+'条回复'" @click-left="$emit('close')">
       <van-icon name="cross" slot="left" />
     </van-nav-bar>
 
+    <!-- 当前评论项 start -->
     <van-cell>
       <comment-item :comment="comment" />
     </van-cell>
+    <!-- 当前评论项 end -->
 
-    <van-cell title="所有回复"></van-cell>
-    <van-cell title="全部评论"></van-cell>
+    <comment-list
+    :source="comment.com_id"
+    type="c"
+    :comment="commentItem"
+    />
 
-  <!-- 底部内容 start -->
-  <div class="article-bottom">
-    <van-button
-    class="comment-btn"
-    type="default"
-    round
-    @click="pubCommentShow = !pubCommentShow"
-    > 写评论 </van-button>
-  </div>
+    <!-- 底部内容 start -->
+    <div class="article-bottom">
+      <van-button
+        class="comment-btn"
+        type="default"
+        round
+        @click="pubCommentShow = !pubCommentShow"
+      >
+        写评论
+      </van-button>
+    </div>
     <!-- 底部内容 end -->
+
+    <!-- 发布评论弹出层组件 start -->
+    <van-popup
+      v-model="pubCommentShow"
+      position="bottom"
+      >
+      <comment-publish @publish="handlePublishComment" />
+    </van-popup>
+    <!-- 发布评论弹出层组件 end -->
   </div>
 </template>
 
 <script>
 import CommentItem from './comment-item'
+import CommentList from './comment-list'
+import CommentPublish from './comment-publish'
+import { addComment } from '@/api/comment'
 export default {
   name: 'CommentReply',
   components: {
-    CommentItem
+    CommentItem,
+    CommentList,
+    CommentPublish
   },
   props: {
     comment: {
       type: Object,
       required: true
+    },
+    target: {
+      type: [Number, String, Object],
+      required: true
+    },
+    art_id: {
+      type: [Number, String, Object],
+      default: null
     }
   },
   data () {
-    return {}
+    return {
+      commentItem: {},
+      pubCommentShow: false
+    }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    console.log(this.comment)
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    // 发布评论
+    async handlePublishComment (content) {
+      this.$loading()
+      try {
+        const { data: response } = await addComment({
+          target: this.comment.com_id,
+          content,
+          art_id: null
+        })
+        this.$toast.success('评论发布成功!')
+        this.pubCommentShow = false
+        this.commentItem = response.data.new_obj
+        this.comment.reply_count++
+      } catch (error) {
+        console.log('评论失败!', error)
+        this.$toast.clear()
+      }
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
-  .comment-reply {
-    padding-bottom: 44px;
-  }
+.comment-reply {
+  padding-bottom: 44px;
+}
 
 .article-bottom {
   display: flex;
