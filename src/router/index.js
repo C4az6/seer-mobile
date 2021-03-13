cc43278c22f0850a7bde0e4e1f10c6cb9f173f4d
c4az6 <1026037632@qq.com>
2021-03-13 21:20:32 +0800

    创建用户编辑页面组件以及配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,51 +3,54 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/login',
-    name: 'login',
-    /* 路由懒加载 */
-    component: () => import('@/views/login')
-  }, {
-    path: '/',
-    // name: '',    如果有子路由，父路由就没必要有名字了，没有意义
-    component: () => import('@/views/layout'),
-    children: [
-      {
-        path: '', // 子路由path为空的情况下会被当作默认子路由
-        name: 'home',
-        component: () => import('@/views/home')
-      },
-      {
-        path: '/qa',
-        name: 'qa',
-        component: () => import('@/views/qa')
-      },
-      {
-        path: '/video',
-        name: 'video',
-        component: () => import('@/views/video')
-      },
-      {
-        path: '/my',
-        name: 'my',
-        component: () => import('@/views/my')
-      }
-    ]
+const routes = [{
+  path: '/login',
+  name: 'login',
+  /* 路由懒加载 */
+  component: () => import('@/views/login')
+}, {
+  path: '/',
+  // name: '',    如果有子路由，父路由就没必要有名字了，没有意义
+  component: () => import('@/views/layout'),
+  children: [{
+    path: '', // 子路由path为空的情况下会被当作默认子路由
+    name: 'home',
+    component: () => import('@/views/home')
   },
   {
-    path: '/search',
-    name: 'search',
-    component: () => import('@/views/search')
+    path: '/qa',
+    name: 'qa',
+    component: () => import('@/views/qa')
   },
   {
-    path: '/article/:articleId',
-    name: 'article',
-    component: () => import('@/views/article'),
-    // 将动态路由参数映射到组件的 props 中，无论是访问还是维护性都很方便
-    props: true
+    path: '/video',
+    name: 'video',
+    component: () => import('@/views/video')
+  },
+  {
+    path: '/my',
+    name: 'my',
+    component: () => import('@/views/my')
   }
+  ]
+},
+{
+  path: '/search',
+  name: 'search',
+  component: () => import('@/views/search')
+},
+{
+  path: '/article/:articleId',
+  name: 'article',
+  component: () => import('@/views/article'),
+  // 将动态路由参数映射到组件的 props 中，无论是访问还是维护性都很方便
+  props: true
+},
+{
+  path: '/user/profile',
+  name: 'user-profile',
+  component: () => import('@/views/user-profile')
+}
 ]
 
 const router = new VueRouter({
---
+++ src/views/user-profile/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="userprofile-wrap">用户编辑页面</div>
+</template>
+
+<script>
+export default {
+  name: 'UserProfileIndex',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
